9657912c23a8b354635c11fa6dadaf7c4ba13f9c
isabelaneu <isabela.neu@germinare.org.br>
2025-03-21 07:11:33 -0300

    Add components e as pages

--- arome/src/App.tsx
+++ arome/src/App.tsx
@@ -2,25 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/home/home';
+import Shop from './pages/shop/shop';
+import Carrinho from './pages/carrinho/carrinho';
+import Product from './pages/product/product';
+import Singup from './pages/singup/singup';
+import Payment from './pages/payment/payment';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path='/carrinho' element={<Carrinho></Carrinho>}/>
+        <Route path='/product' element={<Product></Product>}/>
+        <Route path='/singup' element={<Singup></Singup>}/>
+        <Route path='/payment' element={<Payment></Payment>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
